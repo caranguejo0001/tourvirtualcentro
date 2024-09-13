badedd51ed80844a54a49f4d98fd9f6ff4d8caa9
caranguejo0001 <caranguejo0001@gmail.com>
2024-09-13 05:31:45 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360-aerio-chapado",
+      "name": "360 aerio chapadão",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.6670504949402734,
+        "pitch": 0.004843368488991473,
+        "fov": 1.2836595229737267
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5959902130409951,
+          "pitch": 0.0018727939530940318,
+          "rotation": 0,
+          "target": "3-breachclub-com-trator2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-street-view-360",
+      "name": "Street View 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -2.424333000468108,
+        "pitch": 0.058470147484872115,
+        "fov": 1.2836595229737267
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.030398028732989246,
+          "pitch": 0.12084938498427178,
+          "rotation": 0,
+          "target": "2-breachclub-com-trator"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-breachclub-com-trator",
+      "name": "BREACHCLUB COM TRATOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -1.6479016259412198,
+        "pitch": 0.21649019732690533,
+        "fov": 1.2836595229737267
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.01389659176016167,
+          "pitch": 0.13618205164454622,
+          "rotation": 0,
+          "target": "3-breachclub-com-trator2"
+        },
+        {
+          "yaw": -3.1017236449437036,
+          "pitch": 0.1526821070225921,
+          "rotation": 0,
+          "target": "1-street-view-360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-breachclub-com-trator2",
+      "name": "BREACHCLUB COM TRATOR2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -1.5905436823253076,
+        "pitch": 0.025469434979623173,
+        "fov": 1.2836595229737267
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.052956607457727,
+          "pitch": 0.2310299076037321,
+          "rotation": 0,
+          "target": "2-breachclub-com-trator"
+        },
+        {
+          "yaw": -0.3024271558243523,
+          "pitch": -0.349920028971038,
+          "rotation": 0,
+          "target": "0-360-aerio-chapado"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "centro",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
